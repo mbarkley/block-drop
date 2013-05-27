2fb9182f79686cbb2cdc63abf91e225d9efbd2c8
Max Barkley <mbarkley@redhat.com>
2013-05-27 16:38:49 -0400

    Private: Refactored TTTClient. Moved lobby specific code to Lobby class.

---
+++ src/main/java/ErraiLearning/client/local/Lobby.java
@@ -0,0 +1,152 @@
+package ErraiLearning.client.local;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.errai.bus.client.ErraiBus;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.api.messaging.Message;
+import org.jboss.errai.bus.client.api.messaging.MessageBus;
+import org.jboss.errai.bus.client.api.messaging.MessageCallback;
+import org.jboss.errai.bus.client.api.messaging.RequestDispatcher;
+import org.jboss.errai.common.client.protocols.MessageParts;
+import org.jboss.errai.ui.nav.client.local.DefaultPage;
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.TransitionTo;
+
+import ErraiLearning.client.shared.Invitation;
+import ErraiLearning.client.shared.LobbyUpdate;
+import ErraiLearning.client.shared.LobbyUpdateRequest;
+import ErraiLearning.client.shared.Player;
+import ErraiLearning.client.shared.RegisterRequest;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+@Page(role=DefaultPage.class)
+@ApplicationScoped
+public class Lobby extends Composite {
+	
+	private final class LobbyMessageCallback implements MessageCallback {
+		@Override
+		public void callback(Message message) {
+			if (message.getCommandType().equals("invitation"))
+				invitationCallback(message);
+			else if (message.getCommandType().equals("start-game"))
+				startGameCallback(message);
+		}
+		
+		private void startGameCallback(Message message) {
+			boardTransition.go();
+		}
+
+		private void invitationCallback(Message message) {
+			Invitation invitation = message.get(Invitation.class, MessageParts.Value);
+			
+			invitation.setAccepted(
+				Window.confirm("You have been invited to play a game by "
+						+ invitation.getInviter().getNick()
+						+ ". Would you like to accept?"
+				)
+			);
+			
+			MessageBuilder.createMessage()
+			.toSubject("Relay")
+			.command("invitation-response")
+			.withValue(invitation)
+			.noErrorHandling()
+			.sendNowWith(dispatcher);
+		}
+	}
+	
+	@Inject private TransitionTo<Board> boardTransition;
+	@Inject	private Event<LobbyUpdateRequest> lobbyUpdateRequest;
+	@Inject	private Event<RegisterRequest> registerRequest;
+	@Inject private Event<Invitation> gameInvitation;
+	private MessageBus messageBus = ErraiBus.get();
+	private RequestDispatcher dispatcher = ErraiBus.getDispatcher();
+
+	private VerticalPanel vPanel = new VerticalPanel();
+	private HorizontalPanel buttonPanel = new HorizontalPanel();
+	private VerticalPanel lobbyPanel = new VerticalPanel();
+	private Button lobbyButton = new Button("Join Lobby");
+	
+	/* Shadows lobbyPanel. */
+	private List<Player> lobbyList = new ArrayList<Player>();
+	
+	public Lobby() {
+		initWidget(vPanel);
+		
+		buttonPanel.add(lobbyButton);
+		vPanel.add(buttonPanel);
+		vPanel.add(lobbyPanel);
+		
+		lobbyButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				joinLobby();
+			}
+		});
+	}
+	
+	public void requestLobbyUpdate() {
+		lobbyUpdateRequest.fire(new LobbyUpdateRequest());
+		System.out.println(TTTClient.getInstance().getNickname()+": LobbyUpdateRequest fired.");
+	}
+	
+	public void updateLobby(@Observes LobbyUpdate update) {
+		
+		lobbyList.clear();
+		lobbyPanel.clear();
+	
+		for (Player p : update.getPlayers().values()) {
+			lobbyList.add(p);
+			
+			Button playerButton = new Button(p.getNick());
+			lobbyPanel.add(playerButton);
+			
+			if (p.equals(TTTClient.getInstance().getPlayer()))
+				playerButton.setVisible(false);
+			else
+				playerButton.addClickHandler(new ClickHandler() {
+					@Override
+					public void onClick(ClickEvent event) {
+						Player inviter = TTTClient.getInstance().getPlayer();
+						Player invitee = lobbyList.get(lobbyPanel.getWidgetIndex((Widget) event.getSource()));
+						gameInvitation.fire(new Invitation(inviter, invitee));
+					}
+				});
+		}
+	}
+	
+	public void joinLobby() {
+		RegisterRequest request = new RegisterRequest(TTTClient.getInstance().getNickname());
+		registerRequest.fire(request);
+		System.out.println(TTTClient.getInstance().getNickname()+": LobbyRequest fired.");
+	}
+	
+	public void loadPlayer(@Observes Player player) {
+		TTTClient.getInstance().setPlayer(player);
+		System.out.println(TTTClient.getInstance().getNickname()+": Player object received.");
+		
+		// Hide join lobby button and display join game button.
+		lobbyButton.setVisible(false);
+		
+		System.out.println(TTTClient.getInstance().getNickname()+": Subscribing to subject Client"+player.getId());
+		// Subscribe to server relay
+		messageBus.subscribe("Client"+player.getId(), new LobbyMessageCallback());
+		
+		requestLobbyUpdate();
+	}
+}
--- src/main/java/ErraiLearning/client/local/TTTClient.java
+++ src/main/java/ErraiLearning/client/local/TTTClient.java
@@ -1,124 +1,42 @@
 package ErraiLearning.client.local;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.jboss.errai.bus.client.ErraiBus;
-import org.jboss.errai.bus.client.api.base.MessageBuilder;
-import org.jboss.errai.bus.client.api.messaging.Message;
-import org.jboss.errai.bus.client.api.messaging.MessageBus;
-import org.jboss.errai.bus.client.api.messaging.MessageCallback;
-import org.jboss.errai.bus.client.api.messaging.RequestDispatcher;
-import org.jboss.errai.bus.server.annotations.Command;
-import org.jboss.errai.common.client.api.ErrorCallback;
-import org.jboss.errai.common.client.protocols.MessageParts;
 import org.jboss.errai.ioc.client.api.EntryPoint;
-import org.jboss.errai.ui.nav.client.local.DefaultPage;
 import org.jboss.errai.ui.nav.client.local.Navigation;
-import org.jboss.errai.ui.nav.client.local.Page;
-import org.jboss.errai.ui.nav.client.local.TransitionTo;
 
 import ErraiLearning.client.shared.Invitation;
 import ErraiLearning.client.shared.LobbyUpdateRequest;
-import ErraiLearning.client.shared.RegisterRequest;
-import ErraiLearning.client.shared.LobbyUpdate;
 import ErraiLearning.client.shared.Player;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.LoadEvent;
-import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
-@Page(role=DefaultPage.class)
 @EntryPoint
-public class TTTClient extends Composite {
-	
-	private final class TTTClientMessageCallback implements MessageCallback {
-		@Override
-		public void callback(Message message) {
-			System.out.println(nickname+": Invitation relay received.");
-			
-			if (message.getCommandType().equals("invitation"))
-				invitationCallback(message);
-			else if (message.getCommandType().equals("start-game"))
-				startGameCallback(message);
-		}
-		
-		private void startGameCallback(Message message) {
-			System.out.println("Client"+player.getId()+": Starting game.");
-			
-			boardTransition.go();
-		}
-
-		private void invitationCallback(Message message) {
-			Invitation invitation = message.get(Invitation.class, MessageParts.Value);
-			
-			invitation.setAccepted(
-				Window.confirm("You have been invited to play a game by "
-						+ invitation.getInviter().getNick()
-						+ ". Would you like to accept?"
-				)
-			);
-			
-			MessageBuilder.createMessage()
-			.toSubject("Relay")
-			.command("invitation-response")
-			.withValue(invitation)
-			.noErrorHandling()
-			.sendNowWith(dispatcher);
-		}
-	}
-
-	private VerticalPanel vPanel = new VerticalPanel();
-	private HorizontalPanel buttonPanel = new HorizontalPanel();
-	private VerticalPanel lobbyPanel = new VerticalPanel();
-	private Button lobbyButton = new Button("Join Lobby");
+public class TTTClient {
 	
 	private String nickname;
 	private Player player;
-	/* Shadows lobbyPanel. */
-	private List<Player> lobbyList = new ArrayList<Player>();
 	
-	private MessageBus messageBus = ErraiBus.get();
-	private RequestDispatcher dispatcher = ErraiBus.getDispatcher();
-	@Inject	private Event<RegisterRequest> registerRequest;
-	@Inject	private Event<LobbyUpdateRequest> lobbyUpdateRequest;
-	@Inject private Event<Invitation> gameInvitation;
 	@Inject	private Navigation nav;
-	@Inject private TransitionTo<Board> boardTransition;
 	
 	/* For debugging only. */
 	private static int curDebugId = 1;
 	private int debugId;
 	private static synchronized int nextDebugId() { return curDebugId++; }
 	
+	private static TTTClient instance = null;
+	
+	public static TTTClient getInstance() {
+		return instance;
+	}
+	
 	public TTTClient() {
 		
 		debugId = nextDebugId();
 		System.out.println(nickname+": TTTClient constructor called.");
-		
-		initWidget(vPanel);
-		
-		buttonPanel.add(lobbyButton);
-		vPanel.add(buttonPanel);
-		vPanel.add(lobbyPanel);
 		
 		String initialValue = "Foobar";
 		String msg = "Please select a username.";
@@ -128,73 +46,24 @@ public class TTTClient extends Composite {
 			nickname = "default";
 
 		System.out.println(nickname+": User selected " + nickname + " as their nickname.");
+		
+		instance = this;
 	}
 	
 	@PostConstruct
 	public void postSetup() {
-		
-		lobbyButton.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				joinLobby();
-			}
-		});
-		
 		RootPanel.get().add(nav.getContentPanel());
-	}
-	
-	public void updateLobby(@Observes LobbyUpdate update) {
-		
-		lobbyList.clear();
-		lobbyPanel.clear();
-	
-		for (Player p : update.getPlayers().values()) {
-			lobbyList.add(p);
-			
-			Button playerButton = new Button(p.getNick());
-			lobbyPanel.add(playerButton);
-			
-			if (p.equals(player))
-				playerButton.setVisible(false);
-			else
-				playerButton.addClickHandler(new ClickHandler() {
-					@Override
-					public void onClick(ClickEvent event) {
-						Player inviter = getPlayer();
-						Player invitee = lobbyList.get(lobbyPanel.getWidgetIndex((Widget) event.getSource()));
-						gameInvitation.fire(new Invitation(inviter, invitee));
-						System.out.println(nickname+": Fired invitation request.");
-					}
-				});
-		}
-	}
-	
-	public void loadPlayer(@Observes Player player) {
-		this.player = player;
-		System.out.println(nickname+": Player object received.");
-		
-		// Hide join lobby button and display join game button.
-		lobbyButton.setVisible(false);
-		
-		System.out.println(nickname+": Subscribing to subject Client"+player.getId());
-		// Subscribe to server relay
-		messageBus.subscribe("Client"+player.getId(), new TTTClientMessageCallback());
-		
-		requestLobbyUpdate();
-	}
-
-	public void requestLobbyUpdate() {
-		lobbyUpdateRequest.fire(new LobbyUpdateRequest());
-		System.out.println(nickname+": LobbyUpdateRequest fired.");
-	}
-
-	public void joinLobby() {
-		RegisterRequest request = new RegisterRequest(nickname);
-		registerRequest.fire(request);
-		System.out.println(nickname+": LobbyRequest fired.");
 	}
 	
 	public Player getPlayer() {
 		return player;
+	}
+	
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	public String getNickname() {
+		return nickname;
 	}
 }
