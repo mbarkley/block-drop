0ab76ab5100d26015487416498acfa6fb2cf6a10
Max Barkley <mbarkley@redhat.com>
2013-05-31 14:21:05 -0400

    Added documentation to RegisterRequest class.
    Added comments and javadocs to RegisterRequest class.

--- src/main/java/ErraiLearning/client/shared/RegisterRequest.java
+++ src/main/java/ErraiLearning/client/shared/RegisterRequest.java
@@ -2,25 +2,54 @@ package ErraiLearning.client.shared;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 
+/*
+ * A portable JavaBean class for registering a user with the server and/or
+ * putting them in the lobby.
+ */
 @Portable
 public class RegisterRequest {
 
+	/* The player to be registered. */
 	private Player player = null;
 	
+	/*
+	 * A default no-arg constructor for automatic bean creation. Users should prefer
+	 * the constructor taking a Player argument.
+	 */
 	public RegisterRequest() {}
 	
+	/*
+	 * Create a RegisterRequest object to register player.
+	 * 
+	 * @param player The player to be registered with the server and/or put in the lobby.
+	 */
 	public RegisterRequest(Player player) {
 		this.player = player;
 	}
 
+	/*
+	 * Get the nickname of the player being registered.
+	 * 
+	 * @return The nickname of the player to be registered.
+	 */
 	public String getNickname() {
 		return player.getNick();
 	}
 	
+	/*
+	 * Check if the player has been registered previously.
+	 * 
+	 * @return True iff the player has been registered already.
+	 */
 	public boolean hasRegistered() {
 		return player != null && player.hasRegistered();
 	}
 
+	/*
+	 * Get the player to be registered.
+	 * 
+	 * @return The player to be registered.
+	 */
 	public Player getPlayer() {
 		return player;
 	}
