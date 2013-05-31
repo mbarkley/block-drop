963c0421e1fcb43a4cd42fdb86fa34246359f846
Max Barkley <mbarkley@redhat.com>
2013-05-31 13:36:46 -0400

    Added comment to LobbyUpdateRequest class.
    Added a comment and made a minor formatting edit.

--- src/main/java/ErraiLearning/client/shared/LobbyUpdateRequest.java
+++ src/main/java/ErraiLearning/client/shared/LobbyUpdateRequest.java
@@ -2,6 +2,8 @@ package ErraiLearning.client.shared;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 
+/*
+ * A portable JavaBean for triggering LobbyUpdate events.
+ */
 @Portable
-public class LobbyUpdateRequest {
-}
+public class LobbyUpdateRequest {}
