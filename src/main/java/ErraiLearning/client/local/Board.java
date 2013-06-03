66495a6cd6ac6a4c57881a2c973c36a38ce2a655
Max Barkley <mbarkley@redhat.com>
2013-06-03 15:47:22 -0400

    Documented and formatted the Board class.

--- src/main/java/ErraiLearning/client/local/Board.java
+++ src/main/java/ErraiLearning/client/local/Board.java
@@ -24,15 +24,27 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+/*
+ * An Errai Navigation Page providing the UI for a tic-tac-toe game.
+ */
 @Page
 public class Board extends Composite {
 	
+	/*
+	 * A class for controlling the board based on remote-triggered events.
+	 */
 	public class GameMessageCallback implements MessageCallback {
 
+		/*
+		 * (non-Javadoc)
+		 * @see org.jboss.errai.bus.client.api.messaging.MessageCallback#callback(org.jboss.errai.bus.client.api.messaging.Message)
+		 */
 		@Override
 		public void callback(Message message) {
+			// When a normal move is broadcasted from the server.
 			if ("validate-move".equals(message.getCommandType())) {
 				validateMoveCallback(message);
+			// When a game-ending move is broadcasted from the server.
 			} else if ("game-over".equals(message.getCommandType())) {
 				validateMoveCallback(message);
 				if (game.isOver())
@@ -43,6 +55,12 @@ public class Board extends Composite {
 			}
 		}
 
+		/*
+		 * Handle move broadcasted from server. If this client made the move, ignore it.
+		 * Otherwise, check if the move has been validated by the server and alter the board state.
+		 * 
+		 * This method should only be invoked by the Errai MessageBus.
+		 */
 		private void validateMoveCallback(Message message) {
 			Move move = message.get(Move.class, MessageParts.Value);
 			
@@ -77,20 +95,30 @@ public class Board extends Composite {
 
 	}
 
+	/*
+	 * A tic-tac-toe tile.
+	 */
 	public class TileClickEvent implements ClickHandler {
 
+		/*
+		 * (non-Javadoc)
+		 * @see com.google.gwt.event.dom.client.ClickHandler#onClick(com.google.gwt.event.dom.client.ClickEvent)
+		 */
 		@Override
 		public void onClick(ClickEvent event) {
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": ClickHandler activated.");
+
 			Player activePlayer = TTTClient.getInstance().getPlayer();
-			
 			Tile clickedTile = ((Tile) event.getSource());
 			
 			// Try to make move in model and catch if invalid.
 			try {
 				game.makeMove(activePlayer.getId(), clickedTile.getRow(), clickedTile.getColumn());
+				// For debugging.
 				System.out.println(TTTClient.getInstance().getNickname()+": Move was valid.");
 			} catch (InvalidMoveException e) {
+				// For debugging.
 				System.out.println(TTTClient.getInstance().getNickname()+": Move was invalid.");
 				//TODO: Add friendly error message for user in GUI.
 				return;
@@ -102,6 +130,7 @@ public class Board extends Composite {
 			else
 				setTileToO(clickedTile.getRow(), clickedTile.getColumn());
 			
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": Tile changed.");
 			
 			// Send message to server (to be relayed to other client).
@@ -112,34 +141,50 @@ public class Board extends Composite {
 			.noErrorHandling()
 			.sendNowWith(dispatcher);
 			
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": Move sent.");
 		}
-
 	}
 
+	/* The CSS class name to be assigned to all tiles. */
 	public static final String primaryTileStyle = "tile";
+	/* The CSS class name to be assigned to all vacant tiles. */
 	public static final String unusedTileStyle = "unused-tile";
+	/* The CSS class name to be assigned to all tiles used by player X. */
 	public static final String xTileStyle = "x-tile";
+	/* The CSS class name to be assigned to all tiles used by player O. */
 	public static final String oTileStyle = "o-tile";
 	
+	/* The model for this board. */
 	private Game game = null;
 	
+	/* The base GUI element for displaying this board. */
 	private VerticalPanel boardPanel = new VerticalPanel();
 	
+	/* For sending messages to the server. */
 	@Inject private RequestDispatcher dispatcher;
+	/* For receiving messages from the server. */
 	@Inject private MessageBus messageBus;
 	
+	/*
+	 * This class is the UI for a tic-tac-toe board.
+	 */
 	public Board() {
-		//String nickname = TTTClient.getInstance() != null ? TTTClient.getInstance().getNickname() : "Client";
 		String nickname = TTTClient.getInstance().getNickname();
+		// For debugging.
 		System.out.println(nickname+": Board constructor called.");
 		
 		game = TTTClient.getInstance().getGame();
+		// For debugging.
 		System.out.println(TTTClient.getInstance().getNickname()+": Game object passed to board.");
 		
 		initWidget(boardPanel);
 	}
 	
+	/*
+	 * Called when the game is in a terminal state. Display an appropriate message to the client
+	 * regarding whether they won, lost, or tied.
+	 */
 	public void endGame() {
 		String message;
 		if (game.isDraw())
@@ -152,6 +197,9 @@ public class Board extends Composite {
 		Window.alert(message);
 	}
 
+	/*
+	 * Setup the board display.
+	 */
 	@PostConstruct
 	public void setupBoard() {
 		// Subscribe to game channel, which will be used by server and clients to communicate moves.
@@ -163,8 +211,11 @@ public class Board extends Composite {
 			for (int j = 0; j < 3; j++) {
 				Tile tile = new Tile(i, j);
 				
+				// Assign initial class names, used in CSS file.
 				tile.setStylePrimaryName(primaryTileStyle);
 				tile.setStyleName(unusedTileStyle, true);
+				
+				// Add click handler for user to make move.
 				tile.addClickHandler(new TileClickEvent());
 				
 				hPanel.add(tile);
@@ -173,7 +224,13 @@ public class Board extends Composite {
 	}
 	
 	/*
-	 * Replaces secondary tile style with given style.
+	 * Replaces secondary tile style with given style. Secondary tile styles are unusedTileStyle, xTileStyle,
+	 * or oTileStyle.
+	 * 
+	 * @param row The row index (0-indexed) of the tile to be re-styled.
+	 * @param col The col index (0-indexed) of the tile to be re-styled.
+	 * @param style The CSS class name of the new secondary style. Should be one of unusedTileStyle, xTileStyle,
+	 * or oTileStyle.
 	 */
 	private void setTileStyleSecondary(int row, int col, String style) {
 		SimplePanel tile = (SimplePanel) ((HorizontalPanel) boardPanel.getWidget(row)).getWidget(col);
@@ -181,20 +238,39 @@ public class Board extends Composite {
 		// Clear all styles
 		tile.setStyleName("");
 		
+		// Re-declare base tile style.
 		tile.setStylePrimaryName(primaryTileStyle);
+		// Add new style.
 		tile.setStyleName(style, true);
 	}
 	
+	/*
+	 * Set a tile to display an X.
+	 * 
+	 * @param row The row index (0-indexed) of the tile to be modified.
+	 * @param col The col index (0-indexed) of the tile to be modified.
+	 */
 	public void setTileToX(int row, int col) {
 		setTileStyleSecondary(row, col, xTileStyle);
 	}
 	
+	/*
+	 * Set a tile to display an O.
+	 * 
+	 * @param row The row index (0-indexed) of the tile to be modified.
+	 * @param col The col index (0-indexed) of the tile to be modified.
+	 */
 	public void setTileToO(int row, int col) {
 		setTileStyleSecondary(row, col, oTileStyle);
 	}
 	
+	/*
+	 * Set a tile to appear vacant.
+	 * 
+	 * @param row The row index (0-indexed) of the tile to be modified.
+	 * @param col The col index (0-indexed) of the tile to be modified.
+	 */
 	public void resetTile(int row, int col) {
 		setTileStyleSecondary(row, col, "");
 	}
-
 }
