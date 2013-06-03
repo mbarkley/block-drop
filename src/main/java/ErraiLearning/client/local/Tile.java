0707976f6f0fe50bdcc233ba4be5a1f08bec8df3
Max Barkley <mbarkley@redhat.com>
2013-06-03 16:39:54 -0400

    Documented and formatted Tile class.

--- src/main/java/ErraiLearning/client/local/Tile.java
+++ src/main/java/ErraiLearning/client/local/Tile.java
@@ -2,12 +2,22 @@ package ErraiLearning.client.local;
 
 import com.google.gwt.user.client.ui.FocusPanel;
 
+/*
+ * A UI class for a single tile of a tic-tac-toe board.
+ */
 public class Tile extends FocusPanel {
 	
-	// Both 0-indexed.
+	/* The row index (0-indexed) of this tile in the display. */
 	private int row;
+	/* The col index (0-indexed) of this tile in the display. */
 	private int column;
 	
+	/*
+	 * Construct a tile for a tic-tac-toe board display.
+	 * 
+	 * @param row The row (0-indexed) of this tile in the display.
+	 * @param col The col (0-indexed) of this tile in the display.
+	 */
 	public Tile(int row, int col) {
 		super();
 		
@@ -15,18 +25,39 @@ public class Tile extends FocusPanel {
 		setColumn(col);
 	}
 	
+	/*
+	 * Get the row (0-indexed) of this tile in the display.
+	 * 
+	 * @return The row (0-indexed) of this tile in the display.
+	 */
 	public int getRow() {
 		return row;
 	}
+	
+	/*
+	 * Set the row index (0-indexed) of this tile in the display.
+	 * 
+	 * @param row The row index (0-indexed) of this tile in the display.
+	 */
 	public void setRow(int row) {
 		this.row = row;
 	}
+
+	/*
+	 * Get the column (0-indexed) of this tile in the display.
+	 * 
+	 * @return The column (0-indexed) of this tile in the display.
+	 */
 	public int getColumn() {
 		return column;
 	}
+
+	/*
+	 * Set the column index (0-indexed) of this tile in the display.
+	 * 
+	 * @param column The column index (0-indexed) of this tile in the display.
+	 */
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
-	
 }
