88f46e102543e37a62997eea2a2d64f092526006
Max Barkley <mbarkley@redhat.com>
2013-06-03 16:30:17 -0400

    Documented and formatted Lobby class.

--- src/main/java/ErraiLearning/client/local/Lobby.java
+++ src/main/java/ErraiLearning/client/local/Lobby.java
@@ -35,10 +35,20 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+/*
+ * A class for the UI for a tic-tac-toe Lobby.
+ */
 @Page(role=DefaultPage.class)
 public class Lobby extends Composite {
 	
+	/*
+	 * A class for handling lobby updates and invitation from the server.
+	 */
 	private final class LobbyMessageCallback implements MessageCallback {
+		/*
+		 * (non-Javadoc)
+		 * @see org.jboss.errai.bus.client.api.messaging.MessageCallback#callback(org.jboss.errai.bus.client.api.messaging.Message)
+		 */
 		@Override
 		public void callback(Message message) {
 			if (message.getCommandType().equals("invitation"))
@@ -47,13 +57,21 @@ public class Lobby extends Composite {
 				startGameCallback(message);
 		}
 		
+		/*
+		 * Start a game with another player after a successfully accepted invitation.
+		 */
 		private void startGameCallback(Message message) {
 			TTTClient.getInstance().setGame(message.get(Game.class, MessageParts.Value));
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": Before board transition.");
 			boardTransition.go();
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": After board transition.");
 		}
 
+		/*
+		 * Prompt the user to respond to an invitation from another client.
+		 */
 		private void invitationCallback(Message message) {
 			Invitation invitation = message.get(Invitation.class, MessageParts.Value);
 			
@@ -64,6 +82,7 @@ public class Lobby extends Composite {
 				)
 			);
 			
+			// The server handles the invitation, whether or not it was accepted.
 			MessageBuilder.createMessage()
 			.toSubject("Relay")
 			.command("invitation-response")
@@ -73,29 +92,46 @@ public class Lobby extends Composite {
 		}
 	}
 	
+	/* For the Errai NavigationUI. */
 	@Inject private TransitionTo<Board> boardTransition;
+	/* For requesting lobby updates from the server. */
 	@Inject	private Event<LobbyUpdateRequest> lobbyUpdateRequest;
+	/* For registering this client with the server. */
 	@Inject	private Event<RegisterRequest> registerRequest;
+	/* For inviting another user to play a game. */
 	@Inject private Event<Invitation> gameInvitation;
+	/* For receiving messages from the server. */
 	private MessageBus messageBus = ErraiBus.get();
+	/* For sending messages to the server. */
 	private RequestDispatcher dispatcher = ErraiBus.getDispatcher();
 
+	/* The base UI element for this class. */
 	private VerticalPanel vPanel = new VerticalPanel();
+	/* A panel for buttons used for lobby-related actions. */
 	private HorizontalPanel buttonPanel = new HorizontalPanel();
+	/* The UI element containing the list of users in the lobby. */
 	private VerticalPanel lobbyPanel = new VerticalPanel();
+	/* A button for users to initially join the lobby. */
 	private Button lobbyButton = new Button("Join Lobby");
 	
-	/* Shadows lobbyPanel. */
+	/* Shadows lobbyPanel with corresponding player objects. */
 	private List<Player> lobbyList = new ArrayList<Player>();
 	
+	/*
+	 * Create an instance of a lobby page.
+	 */
 	public Lobby() {
 		initWidget(vPanel);
 	}
 	
+	/*
+	 * Construct the UI elements for the lobby.
+	 */
 	@PostConstruct
 	public void postConstruct() {
 		vPanel.add(lobbyPanel);
 		
+		// If this user has not been to the lobby previously, display a button to join the lobby.
 		if (TTTClient.getInstance().getPlayer() == null) {
 			buttonPanel.add(lobbyButton);
 			vPanel.add(buttonPanel);
@@ -110,22 +146,32 @@ public class Lobby extends Composite {
 		}
 	}
 	
+	/*
+	 * Request an update of the current clients in the lobby from the server.
+	 */
 	public void requestLobbyUpdate() {
 		lobbyUpdateRequest.fire(new LobbyUpdateRequest());
 		System.out.println(TTTClient.getInstance().getNickname()+": LobbyUpdateRequest fired.");
 	}
 	
+	/*
+	 * Update the lobby list model and display with newest lobby update from the server.
+	 */
 	public void updateLobby(@Observes LobbyUpdate update) {
 		
+		// Out with the old.
 		lobbyList.clear();
 		lobbyPanel.clear();
-	
+
 		for (Player p : update.getPlayers().values()) {
+			// Add to model.
 			lobbyList.add(p);
 			
+			// Add to view.
 			Button playerButton = new Button(p.getNick());
 			lobbyPanel.add(playerButton);
 			
+			// Don't display this user in the list.
 			if (p.equals(TTTClient.getInstance().getPlayer()))
 				playerButton.setVisible(false);
 			else
@@ -140,23 +186,34 @@ public class Lobby extends Composite {
 		}
 	}
 	
+	/*
+	 * Register this user with the lobby.
+	 */
 	public void joinLobby() {
+		// If the user is joinging for the first time, make a new player object.
 		Player player = TTTClient.getInstance().getPlayer() != null 
 				? TTTClient.getInstance().getPlayer(): new Player(TTTClient.getInstance().getNickname());
 		RegisterRequest request = new RegisterRequest(player);
 		registerRequest.fire(request);
+		// For debugging.
 		System.out.println(TTTClient.getInstance().getNickname()+": LobbyRequest fired.");
 	}
 	
+	/*
+	 * Accept a player object from the server as the canonical representation of this user.
+	 */
 	public void loadPlayer(@Observes Player player) {
+		// For debugging.
 		System.out.println(TTTClient.getInstance().getNickname()+": Player object received.");
 		
 		// Hide join lobby button.
 		lobbyButton.setVisible(false);
 		
+		// If this user has not yet been registered, subscribe to server relay
 		if (!TTTClient.getInstance().hasRegisteredPlayer()) {
+			// For debugging.
 			System.out.println(TTTClient.getInstance().getNickname()+": Subscribing to subject Client"+player.getId());
-			// Subscribe to server relay
+
 			messageBus.subscribe("Client"+player.getId(), new LobbyMessageCallback());
 		}
 		
