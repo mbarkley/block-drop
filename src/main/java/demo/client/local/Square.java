d178862af6f0a46de5d81299c64aae21cac3c0a0
Max Barkley <mbarkley@redhat.com>
2013-06-10 11:44:11 -0400

    Added documentation to Square class.

--- src/main/java/demo/client/local/Square.java
+++ src/main/java/demo/client/local/Square.java
@@ -2,10 +2,16 @@ package demo.client.local;
 
 import com.google.gwt.canvas.dom.client.Context2d;
 
+/*
+ * A class for drawing individual squares in a Block Drop block.
+ */
 public class Square {
 	
+	/* The width and height (in pixels) of each square. */
 	public static final int SIZE = 60;
+	/* The default colour of the interior of a square. */
 	public static final String INTERIOR_DEFAULT = "red";
+	/* The default colour of the outline of each square. */
 	public static final String OUTLINE_DEFAULT = "black";
 
 	/* The colour of the interior of the block. */
@@ -13,24 +19,47 @@ public class Square {
 	/* The colour of the block outline. */
 	private String outlineColour;
 	
+	/*
+	 * Create a Square instance with default colours.
+	 */
 	public Square() {
 		this(INTERIOR_DEFAULT, OUTLINE_DEFAULT);
 	}
 	
+	/*
+	 * Create a Square instance with specified interior colour.
+	 * 
+	 * @param interiorColour The colour of the interior of this square.
+	 */
 	public Square(String interiorColour) {
 		this(interiorColour, OUTLINE_DEFAULT);
 	}
 	
+	/*
+	 * Create a Square instance with specified interior and outline colours.
+	 * 
+	 * @param interiorColour The colour of this Square's interior.
+	 * @param outlineColour The colour of this Square's outline.
+	 */
 	public Square(String interiorColour, String outlineColour) {
 		this.interiorColour = interiorColour;
 		this.outlineColour = outlineColour;
 	}
 	
+	/*
+	 * Draw this Square onto an HTML5 canvas.
+	 * 
+	 * @param x The x coordinate of the position to draw this square.
+	 * @param y The y coordinate of the position to draw this square.
+	 * @param context2d The context of the canvas on which this square will be drawn.
+	 */
 	public void draw(double x, double y, Context2d context2d) {
+		// Draw the outline.
 		makeOuterRectPath(x, y, context2d);
 		context2d.setFillStyle(outlineColour);
 		context2d.fill();
 		
+		// Draw the interior.
 		makeInnerRectPath(x, y, context2d);
 		context2d.setFillStyle(interiorColour);
 		context2d.fill();
@@ -41,12 +70,26 @@ public class Square {
 		context2d.rect(x, y, SIZE, SIZE);
 	}
 	
+	/*
+	 * Select a rectangle path enclosing an entire square.
+	 * 
+	 * @param x The x coordinate of the rectangle.
+	 * @param y The y coordinate of the rectangle.
+	 * @param context2d The context of the canvas on which to make the rectangle path.
+	 */
 	private void makeOuterRectPath(double x, double y, Context2d context2d) {
 		context2d.beginPath();
 		context2d.rect(x, y, SIZE, SIZE);
 		context2d.closePath();
 	}
 	
+	/*
+	 * Select a rectangle path enclosing the interior (not the outline) of this square.
+	 * 
+	 * @param x The x coordinate of the square's position.
+	 * @param y The y coordinate of the sqaure's position.
+	 * @param context2d The context of the canvas on which to select this path.
+	 */
 	private void makeInnerRectPath(double x, double y, Context2d context2d) {
 		context2d.beginPath();
 		context2d.rect(x+1, y+1, SIZE-2, SIZE-2);
